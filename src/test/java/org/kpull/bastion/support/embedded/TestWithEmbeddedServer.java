77108e325c85e50c6f50bcb0700bda86a7bd7f1d
Francesco <frellibb@gmail.com>
2016-05-22 21:20:49 +0200

    Made TestWithEmbeddedServer create a static web service using JUnit BeforeClass and AfterClass annotations

--- src/test/java/org/kpull/bastion/support/embedded/TestWithEmbeddedServer.java
+++ src/test/java/org/kpull/bastion/support/embedded/TestWithEmbeddedServer.java
@@ -1,26 +1,34 @@
 package org.kpull.bastion.support.embedded;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import static java.lang.String.format;
 
 /**
  * To be extended by tests which will require a testing web service to make REST API calls to.
- * The web service will be started and stopped on the provided port before and and after each test method.
+ * The web service will be started on port 9876 and stopped before and after all tests are run.
  */
 public abstract class TestWithEmbeddedServer {
 
-    protected abstract int getEmbeddedServerPort();
+    private static final String host = "http://localhost";
+    private static final int port = 9876;
+    private static final String path = "/sushi";
 
-    private SushiService sushiService;
+    private static SushiService sushiService;
 
-    @Before
-    public  void before() {
-        sushiService = new SushiService(getEmbeddedServerPort());
+    protected String getUrl() {
+        return format("%s:%s%s", host, port, path);
+    }
+
+    @BeforeClass
+    public static void startService() {
+        sushiService = new SushiService(port);
         sushiService.start();
     }
 
-    @After
-    public void after() {
+    @AfterClass
+    public static void stopService() {
         sushiService.stop();
     }
 }
