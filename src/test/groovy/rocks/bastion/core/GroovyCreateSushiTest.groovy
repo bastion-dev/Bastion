aa5338748e7dcf6f6a0bb228fbd8ac278e7df0de
Kyle Pullicino <mail@kylepullicino.com>
2016-07-07 20:04:33 +0200

    Added Groovy script equivalent of CreateSushiTest

---
+++ src/test/groovy/rocks/bastion/core/GroovyCreateSushiTest.groovy
@@ -0,0 +1,31 @@
+package rocks.bastion.core
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import rocks.bastion.core.assertions.JsonResponseAssertions
+import rocks.bastion.core.request.JsonRequest
+import rocks.bastion.junit.BastionRunner
+import rocks.bastion.support.embedded.TestWithEmbeddedServer
+
+@RunWith(BastionRunner.class)
+public class CreateSushiTest extends TestWithEmbeddedServer {
+
+    @Test
+    public void secondTestCreateSushi_Success() {
+        Bastion.api("First Request", JsonRequest.postFromString("http://localhost:9876/sushi",
+                '''{
+                "name":"sashimi",
+                "price":"5.60",
+                "type":"SASHIMI"
+            }'''
+        )).withAssertions(JsonResponseAssertions.fromString(201,
+                '''{
+               "id":5,
+               "name":"sashimi",
+               "price":5.60,
+               "type":"SASHIMI"
+            }'''
+        ).ignoreFieldsValues("/id")
+        ).call()
+    }
+}
