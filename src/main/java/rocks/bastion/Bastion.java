d2c62d7f91b4baf1a54689746d0445d58e54f3ad
Kyle Pullicino <mail@kylepullicino.com>
2016-07-25 21:35:34 +0200

    Added documentation.

--- src/main/java/rocks/bastion/Bastion.java
+++ src/main/java/rocks/bastion/Bastion.java
@@ -76,9 +76,75 @@ import rocks.bastion.core.builder.PostExecutionBuilder;
  * <p>Obtaining the response in this way is useful especially if you want to perform another Bastion request after in the same
  * test. You can use the information retrieved in the model or response for the next Bastion request.</p>
  * <h1>Request Types</h1>
+ * <p>
+ * There are multiple types of requests that you can send using Bastion. To start building a Bastion test, construct and initialise
+ * a request and pass it to the {@link #request(String, HttpRequest)} method. The types of {@link HttpRequest requests} you can
+ * use are listed below (see each requests's JavaDocs for more information about how to use that request):
+ * </p>
+ * <ul>
+ * <li>{@link rocks.bastion.core.GeneralRequest}: Allows you to specify a string content-body. Can also be used if you wouldn't
+ * like to send any content-body (such as for a {@code GET} request).</li>
+ * <li>{@link rocks.bastion.core.json.JsonRequest}: Allows you to specify a valid JSON string to send as part of your HTTP request
+ * content-body.</li>
+ * </ul>
+ * <p>Also, feel free to provide your own implementation of an {@link HttpRequest} which will allow you customise completely
+ * the request sent by Bastion. Implementing your own {@link HttpRequest request class} is extremely useful to encourage
+ * reuse and maintainability (instead of repeating your own requests each time).</p>
+ * <p>
  * <h1>Assertion Types</h1>
+ * <p>
+ * Assertions allow you to quickly check whether a test was successful or not. Note that a request does not necessarily have
+ * to succeed for a test to pass. In fact, you are encouraged to test your error/failure paths extensively as well as your
+ * success paths of your APIs to ensure good quality software. Bastion allows you to do this easily by supply an {@link Assertions}
+ * object to the {@link rocks.bastion.core.builder.AssertionsBuilder#withAssertions(Assertions)}} method. At the moment, there
+ * is only one built-in {@link Assertions assertion type} listed below (see the type's JavaDocs for more information on
+ * how to use it):
+ * </p>
+ * <ul>
+ * <li>{@link rocks.bastion.core.json.JsonResponseAssertions}: Expects a JSON response from the remote server. It allows
+ * you to specify a valid JSON string to assert the response against. The assertion is smart in that a JSON structural comparison
+ * is performed (instead of a straight-up text equality comparison) making sure that the response contains all the expected
+ * properties, with the correct values, regardless of whitespace and property order.</li>
+ * </ul>
+ * <p>
+ * Just like requests, you may define your own {@link Assertions assertion types}. You are encouraged to do so if you
+ * are performing some sort of assertion frequently throughout your tests. This enables code reuse and improves the maintainability
+ * of your tests.
+ * </p>
+ * <p>
+ * If you do not want to implement your own assertion classes you can supply an assertions object directly into the
+ * {@link rocks.bastion.core.builder.AssertionsBuilder#withAssertions(Assertions)} method of the Bastion test builder as
+ * a Java 8 lambda.
+ * </p>
  * <h1>Callbacks</h1>
+ * <p>
+ * {@link Callback Callback objects} can be provided for a Bastion test using the {@link rocks.bastion.core.builder.CallbackBuilder#thenDo(Callback)}
+ * method. Callbacks are executed right after the test passes its assertions. Using a callback you may perform additional
+ * post processing to the response object which is not necessarily related to test assertions. This can include logging,
+ * setting variables for use further in your test and so on.
+ * </p>
+ * <p>You are encouraged to define your own {@link Callback callback} implementations. Just like requests and assertions,
+ * this promotes code reuse and maintainability especially if you are performing an action multiple times across your
+ * Bastion tests.</p>
+ * <p>
+ * If you do not want to implement your own callback classes though you can supply a callback object directly into the
+ * {@link rocks.bastion.core.builder.CallbackBuilder#thenDo(Callback)} method of the Bastion test builder as a Java 8
+ * lambda.
+ * </p>
  * <h1>Groovy Tests</h1>
+ * <p>
+ * Certain features of Bastion such as the {@link rocks.bastion.core.json.JsonRequest} and the {@link rocks.bastion.core.json.JsonResponseAssertions}
+ * classes work with strings. These string inputs will frequently turn out to be extremely long and, especially in Java,
+ * this will cause your strings to appear unwieldy due to multiple breaklines and escape characters. Fortunately, Bastion
+ * works with any of the JVM languages including Groovy.
+ * </p>
+ * <p>
+ * Groovy is easy to set up for your project. After you have set up Groovy to run automated tests, simply add Bastion to
+ * your project's dependencies (as explained above) and create your tests as normal. With Groovy, you can supply long strings
+ * using alternate string delimiters including so-called multiline strings. These will improve the readability of your code
+ * greatly when suppying strings to Bastion. See the <a href="https://bastion.rocks">Bastion User Guide</a>
+ * for an example of how a Bastion test would look like in Groovy.
+ * </p>
  */
 public final class Bastion {
 
