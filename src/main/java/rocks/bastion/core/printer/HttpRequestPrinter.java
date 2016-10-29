0b47327fe455b6879e28c5fc4b1af4e4d26f75f3
Kyle Pullicino <mail@kylepullicino.com>
2016-10-29 15:13:39 +0200

    #30: Code cleanup and added documentation.

--- src/main/java/rocks/bastion/core/printer/HttpRequestPrinter.java
+++ src/main/java/rocks/bastion/core/printer/HttpRequestPrinter.java
@@ -1,51 +1,57 @@
 package rocks.bastion.core.printer;
 
-import com.mashape.unirest.http.HttpMethod;
 import org.apache.http.ProtocolVersion;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicLineFormatter;
 import org.apache.http.message.BasicRequestLine;
-import org.apache.http.message.LineFormatter;
+import rocks.bastion.core.ApiHeader;
 import rocks.bastion.core.HttpRequest;
 import rocks.bastion.core.RequestExecutor;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.net.URL;
+import java.util.Collection;
 import java.util.Objects;
 
 /**
- * @author <a href="mailto:mail@kylepullicino.com">Kyle</a>
+ * Prints the given {@link HttpRequest} object in the same format that it is sent with using the HTTP protocol. This class is
+ * useful for logging and debugging purposes.
  */
 public class HttpRequestPrinter {
 
     private HttpRequest request;
 
+    /**
+     * Constructs a new instance of this printer to format the specified request object.
+     *
+     * @param request The request object to format. Cannot be {@literal null}.
+     */
     public HttpRequestPrinter(HttpRequest request) {
         Objects.requireNonNull(request);
         this.request = request;
     }
 
+    /**
+     * Formats and prints the request object to the specified {@link Writer} stream. The format used when outputting the
+     * response conforms to the HTTP standard.
+     *
+     * @param writer The writer to output to. Cannot be {@link null}.
+     * @throws IOException Thrown if the request could not be formatted and sent to the specified writer.
+     */
     public void print(Writer writer) throws IOException {
-        RequestExecutor executor = new RequestExecutor(request);
-        URL url = new URL(executor.getResolvedUrl());
-        BasicRequestLine requestLine = new BasicRequestLine(executor.getMethod(), url.getFile(), new ProtocolVersion("HTTP", 1, 1));
-        BasicLineFormatter formatter = new BasicLineFormatter();
-        writer.append(BasicLineFormatter.formatRequestLine(requestLine, formatter)).append("\r\n");
-        writer.append(BasicLineFormatter.formatHeader(new BasicHeader("Host", url.getHost()), formatter)).append("\r\n");
-        executor.getHeaders().forEach(apiHeader -> {
-            try {
-                writer.append(BasicLineFormatter.formatHeader(new BasicHeader(apiHeader.getName(), apiHeader.getValue()), formatter)).append("\r\n");
-            } catch (IOException exception) {
-                throw new IllegalStateException(exception);
-            }
-        });
-        writer.append("\r\n");
-        writer.append(request.body().toString());
+        Objects.requireNonNull(writer);
+        writeHeadSection(writer);
+        writeEntitySection(writer);
     }
 
+    /**
+     * Formats and returns the request object. The format produced by this method for the request conforms to the HTTP
+     * standard.
+     *
+     * @return A string representation of the HTTP data received
+     */
     public String getAsString() {
         StringWriter writer = new StringWriter();
         try {
@@ -54,6 +60,35 @@ public class HttpRequestPrinter {
             throw new IllegalStateException("Could not convert the HTTP request to a string", exception);
         }
         return writer.toString();
+    }
+
+    private void writeHeadSection(Writer writer) throws IOException {
+        RequestExecutor executor = new RequestExecutor(request);
+        URL url = new URL(executor.getResolvedUrl());
+        BasicLineFormatter formatter = new BasicLineFormatter();
+        writeRequestLine(url, writer, formatter);
+        writeHeaders(url, executor.getHeaders(), writer, formatter);
+        writer.append("\r\n");
+    }
+
+    private void writeHeaders(URL url, Collection<ApiHeader> headers, Writer writer, BasicLineFormatter formatter) throws IOException {
+        writer.append(BasicLineFormatter.formatHeader(new BasicHeader("Host", url.getHost()), formatter)).append("\r\n");
+        headers.forEach(apiHeader -> {
+            try {
+                writer.append(BasicLineFormatter.formatHeader(new BasicHeader(apiHeader.getName(), apiHeader.getValue()), formatter)).append("\r\n");
+            } catch (IOException exception) {
+                throw new IllegalStateException(exception);
+            }
+        });
+    }
+
+    private void writeRequestLine(URL url, Writer writer, BasicLineFormatter formatter) throws IOException {
+        BasicRequestLine requestLine = new BasicRequestLine(request.method().getValue(), url.getFile(), new ProtocolVersion("HTTP", 1, 1));
+        writer.append(BasicLineFormatter.formatRequestLine(requestLine, formatter)).append("\r\n");
+    }
+
+    private void writeEntitySection(Writer writer) throws IOException {
+        writer.append(request.body().toString());
     }
 
 }
--- src/main/java/rocks/bastion/core/printer/ResponsePrinter.java
+++ src/main/java/rocks/bastion/core/printer/ResponsePrinter.java
@@ -5,7 +5,6 @@ import org.apache.http.ProtocolVersion;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicLineFormatter;
 import org.apache.http.message.BasicStatusLine;
-import org.springframework.util.StreamUtils;
 import rocks.bastion.core.Response;
 
 import java.io.IOException;
@@ -15,34 +14,42 @@ import java.io.Writer;
 import java.util.Objects;
 
 /**
- * @author <a href="mailto:mail@kylepullicino.com">Kyle</a>
+ * Prints the given {@link Response} object in the same format that it is sent with using the HTTP protocol. This class is
+ * useful for logging and debugging purposes.
  */
 public class ResponsePrinter {
 
     private Response response;
 
+    /**
+     * Constructs a new instance of this printer to format the specified response object.
+     *
+     * @param response The response object to format. Cannot be {@literal null}.
+     */
     public ResponsePrinter(Response response) {
         Objects.requireNonNull(response);
         this.response = response;
     }
 
+    /**
+     * Formats and prints the response object to the specified {@link Writer} stream. The format used when outputting the
+     * response conforms to the HTTP standard.
+     *
+     * @param writer The writer to output to. Cannot be {@link null}.
+     * @throws IOException Thrown if the response could not be formatted and sent to the specified writer.
+     */
     public void print(Writer writer) throws IOException {
-        BasicLineFormatter formatter = new BasicLineFormatter();
-        String statusLine = BasicLineFormatter.formatStatusLine(new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1),
-                response.getStatusCode(), response.getStatusText()), formatter);
-        writer.append(statusLine).append("\r\n");
-        response.getHeaders().forEach(apiHeader -> {
-            try {
-                writer.append(BasicLineFormatter.formatHeader(new BasicHeader(apiHeader.getName(), apiHeader.getValue()), formatter)).append("\r\n");
-            } catch (IOException exception) {
-                throw new IllegalStateException(exception);
-            }
-        });
-        writer.append("\r\n");
-        InputStreamReader entity = new InputStreamReader(response.getBody());
-        CharStreams.copy(entity, writer);
+        Objects.requireNonNull(writer);
+        writeHeadSection(writer);
+        writeEntitySection(writer);
     }
 
+    /**
+     * Formats and returns the response object. The format produced by this method for the response conforms to the HTTP
+     * standard.
+     *
+     * @return A string representation of the HTTP data received
+     */
     public String getAsString() {
         StringWriter writer = new StringWriter();
         try {
@@ -51,6 +58,34 @@ public class ResponsePrinter {
             throw new IllegalStateException("Could not convert the HTTP request to a string", exception);
         }
         return writer.toString();
+    }
+
+    private void writeHeadSection(Writer writer) throws IOException {
+        BasicLineFormatter formatter = new BasicLineFormatter();
+        writeStatusLine(writer, formatter);
+        writeHeaders(writer, formatter);
+        writer.append("\r\n");
+    }
+
+    private void writeHeaders(Writer writer, BasicLineFormatter formatter) {
+        response.getHeaders().forEach(apiHeader -> {
+            try {
+                writer.append(BasicLineFormatter.formatHeader(new BasicHeader(apiHeader.getName(), apiHeader.getValue()), formatter)).append("\r\n");
+            } catch (IOException exception) {
+                throw new IllegalStateException(exception);
+            }
+        });
+    }
+
+    private void writeStatusLine(Writer writer, BasicLineFormatter formatter) throws IOException {
+        String statusLine = BasicLineFormatter.formatStatusLine(new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1),
+                response.getStatusCode(), response.getStatusText()), formatter);
+        writer.append(statusLine).append("\r\n");
+    }
+
+    private void writeEntitySection(Writer writer) throws IOException {
+        InputStreamReader entity = new InputStreamReader(response.getBody());
+        CharStreams.copy(entity, writer);
     }
 
 }
